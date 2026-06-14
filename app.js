55c7cbadaedbe3035021193f4400ca266e9ecd90
HardikYadav99 <hardikyadav1567@gmail.com>
2026-06-14 17:44:17 +0530

    trigger: triggering git pipeline

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('Pipeline is working \n');
+    res.end('User App is Deployed \n');
 });
 
 server.listen(3000, () => {
