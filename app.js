7411bdfa6c378d216907c6eb6f9f9f96c257a565
HardikYadav99 <hardikyadav1567@gmail.com>
2026-06-15 10:22:45 +0530

    trigger: actions trigger check

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('User App is Deployed \n');
+    res.end('User App is Deployed with updated backend \n');
 });
 
 server.listen(3000, () => {
